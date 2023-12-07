4da8c1bf49fe34c3926387cf616b3cc97d59b63c
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-07 12:29:46 -0300

    add  navbar

--- src/app/components/SearchInput.jsx
+++ src/app/components/SearchInput.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './SearchInput.css'
+import SearchSVG from '../SVG/SearchSVG'
 
 const SearchInput = () => {
-  return (
-    <>
-    <div className='input__container'>
-        <input  type="text" />
-        <span className='input__icon'>🔍</span>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className='input__container'>
+                <span className='input__icon'>
+                    <SearchSVG width={'40px'} height={'40px'} fill={'#696969'}></SearchSVG>
+                </span>
+                <input type="text" />
+            </div>
+        </>
+    )
 }
 
 export default SearchInput
--- src/app/containers/Navbar.jsx
+++ src/app/containers/Navbar.jsx
@@ -2,32 +2,41 @@ import React from 'react'
 import './Navbar.css'
 // import SearchInput from '@components/SearchInput.jsx'
 import SearchInput from '../components/SearchInput'
+import MenuSVG from '../SVG/MenuSVG'
+import CartSVG from '../SVG/CartSVG'
+import Image from 'next/image';
 
 const Navbar = () => {
-  return (
-    <nav className='nav__container'>
-        <div>Logo </div>
-        <div>ubicación</div>
-        <SearchInput></SearchInput>
-        <ul>
+    return (
+        <nav className='nav__container'>
+            <img className='comerce__icon'   src='/img/open-trade-1.jpg' alt='Opentrade icon' />
+            <SearchInput></SearchInput>
+                <div className='mobile__menu' alt='search bar'>
+                    <MenuSVG width={'40px'}  height={'40px'} fill={'#696969'} ></MenuSVG>
+                    <CartSVG width={'40px'}  height={'40px'}  fill={'#696969'}></CartSVG>
+                </div>
+
+            {/* <div>ubicación</div> */}
+            {/*         <ul>
             <li>Categorias</li>
             <li>Ofertas</li>
             <li>Historial</li>
             <li>Supermercado</li>
             <li>Vender</li>
             <li>Ayuda</li>
-        </ul>
-        <span>imagen promocional</span>
-        <ul>
+        </ul> */}
+            {/* <span>imagen promocional</span> */}
+
+            {/*         <ul className='personal-menu'>
             <li>Crea tu cuenta</li>
             <li>Ingresá</li>
             <li>Mis compras</li>
             <li>🛒</li>
-        </ul>
-    </nav>
+        </ul> */}
+        </nav>
 
 
-  )
+    )
 }
 
 export default Navbar

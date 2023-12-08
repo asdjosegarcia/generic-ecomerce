15ec68e2af84bd6bd90be243e1a1120672f405e8
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-08 10:29:06 -0300

    add GET request by categories

---
+++ src/app/api/products/categories/[categories]/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+// import { prisma }  from '@/libs/prisma' //traemos la calse que instanciamos(prismaclient) en prima.js
+// import { Prisma } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+export async function GET(request, { params }) {
+    console.log(params)
+    const products = await prisma.product.findMany({//encontrar unica 
+        where: { //were se usa para buscar lo que coincida
+            categories: {
+                path: [params.categories], //filtramos los articulos que tengan la categorio ingresada en la url 
+                not:null //cualquier articulo que NO sea nomre_caegoria:null
+
+                // equals: 'summer',
+                // equals:{{"summer"}}
+                
+            }
+        }
+    })
+    // console.log(products)
+    return NextResponse.json(products)//mostramos la tarea en pantalla
+
+}

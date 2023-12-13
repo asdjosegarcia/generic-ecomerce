ad7ae5c23322cb30bfeb95caffe6fbaf56756d20
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-13 18:21:18 -0300

    add 2 parameters GET request filterBY DB add animations to filter switchs

--- src/app/api/products/filter-by/[filterBy]/route.js
+++ src/app/api/products/filter-by/[filterBy]/route.js
@@ -3,32 +3,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function GET(request, { params }) {
-    let property = ''
-    let value = 'not:null'
-    console.log(params.filterBy)
-    // let 
-    switch (true) {
-        case (params.filterBy == 'free-shipping'):
-            property = 'shipment'
-            value = 0
-            break;
-        case (params.filterBy == 'new'):
-            property = 'condition'
-            value='New'
-            break;
-        // case (params.filterBy == 'official-stores'):
-        //     value = ''
-        //     property = 'views'
-        //     break;
-        default:
-
+    let property = []//
+    if(params.filterBy.includes('freeShipping')){
+        console.log('free')
+        property.push({ shipment:0,})
     }
+    if(params.filterBy.includes('new')){
+        console.log('new')
+        property.push({condition:'New'})
+    }
+
     const products = await prisma.product.findMany({//buscar todos los articulos que...
         take: 10, // limitamos los resultados a 10
         where: {//donde..
-            [property]: value, // propiedad y valor correspondan 
+            AND: property, // propiedad y valor correspondan 
         },
     })
-    console.log(products)
+
     return NextResponse.json(products)
 }
--- src/app/components/FilterMenu.jsx
+++ src/app/components/FilterMenu.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
 import ToggleOffSVG from '../SVG/ToggleOffSVG'
 import ToggleOnSVG from '../SVG/ToggleOnSVG'
 import './FilterMenu.css'
+import { variableContext } from "../context/contexto";
 
 
 const FilterMenu = () => {
+  const [getIsActive,setIsActive]=useState(
+    {
+      freeShipping:false,
+      new:false,
+    }
+  )
+  const contexto = useContext(variableContext)
+
   const filterCliked=(clickValue)=>{
-    console.log(clickValue)
+    contexto.setProductListURL(`/api/products/filter-by/${clickValue}`)
+    setIsActive({...getIsActive,[clickValue]:!getIsActive[clickValue]})
+    // console.log(getIsActive)
+      
   }
 
 
   return (
     <div className='filter-menu-container'>
-      <button className='toggle-button' onClick={()=>filterCliked('free-shipment')}>
+      <button className='toggle-button' onClick={()=>filterCliked('freeShipping')}>
         <p>Free Shipping ㅤㅤㅤㅤ</p>
-        <ToggleOffSVG width={'48px'} fill={'#696969'}></ToggleOffSVG>
+        {getIsActive.freeShipping?<ToggleOnSVG width={'48px'} fill={'#008000'}></ToggleOnSVG>:<ToggleOffSVG width={'48px'} fill={'#696969'}></ToggleOffSVG>}
+        
         {/* <ToggleOnSVG></ToggleOnSVG> */}
       </button>
-      <button className='toggle-button' onClick={()=>filterCliked('New')}>
+      <button className='toggle-button' onClick={()=>filterCliked('new')}>
         <p>New ㅤㅤㅤㅤ</p>
-        <ToggleOffSVG width={'48px'} fill={'#696969'}></ToggleOffSVG>
-        {/* <ToggleOnSVG></ToggleOnSVG> */}
+        {getIsActive.new?<ToggleOnSVG width={'48px'} fill={'#008000'}></ToggleOnSVG>:<ToggleOffSVG width={'48px'} fill={'#696969'}></ToggleOffSVG>}
       </button>
       <button className='toggle-button'onClick={()=>filterCliked('official')}> 
         <p>Official Stores ㅤㅤㅤㅤ</p>

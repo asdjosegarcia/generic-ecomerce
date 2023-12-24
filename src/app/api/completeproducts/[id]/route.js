f7cfe367bb7863f4bb7c053ec494d7c15247fd5c
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-24 04:55:13 -0300

    add ProductComplete GET request to DB

--- src/app/api/completeproducts/[id]/route.js
+++ src/app/api/completeproducts/[id]/route.js
@@ -1,4 +1,20 @@
-const { PrismaClient } = require('@prisma/client');
+import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export async function GET(request, { params }) {
+    const porductcomplete = await prisma.product.findUnique({//encontrar unico
+        where: { //were se usa para buscar lo que coincida
+            id: Number(params.id)//le decimos que busque lo que corresponda con lo que le mandamos en params.id
+        },
+        include: { ProductComplete: true },
+
+    })
+    console.log(porductcomplete)
+    return NextResponse.json(porductcomplete)//mostramos la tarea en pantalla
+}
+
+/* const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
@@ -20,3 +36,4 @@ getProductCompleteForProduct5()
   .finally(async () => {
     await prisma.$disconnect();
   });
+ */

d4133e6a68b1fb82a559831495b608a01e17fecc
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-24 22:27:53 -0300

    add Product compllete POST request to DB

--- src/app/api/completeproducts/route.js
+++ src/app/api/completeproducts/route.js
@@ -3,39 +3,43 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 
-export async function GET(){
+export async function GET() {
   console.log('hola')
-    const products=await prisma.product.findMany({//iniciamos la 
-        include: {
-            ProductComplete: true,//nos incluye los completeproduct en cada objeto
-          },
-        }) 
-    // console.log(products)
-    return NextResponse.json(products)
+  const products = await prisma.product.findMany({//iniciamos la 
+    include: {
+      ProductComplete: true,//nos incluye los completeproduct en cada objeto
+    },
+  })
+  // console.log(products)
+  return NextResponse.json(products)
 }
 
-export async function POST(request){
-    const {title,description,price,images,condition,shipment,qualification,categories,seller,}=await request.json()//transformamos la peticion a json a js, y se almacenan en title y description
-    const newProduct=await prisma.product.create({//linea para crear datos en nuestra base de datos
-            data: {
-                id:12, // Asigna el valor correspondiente para el nuevo producto complet
-                stock: 10, // Asigna el valor correspondiente para el stock
-                description: 'Descripción del producto', // Asigna la descripción del producto si es necesario
-                // Otras propiedades aquí...
-                images: {
-                  image1: 'URL de la imagen 1',
-                  // Otras imágenes si es necesario...
-                },
-                colors: {
-                  color1: 'Rojo',
-                  // Otros colores si es necesario...
-                },
-                
-                addComplete: {
-                  connect: { id: 12 }, // Conecta este producto completo al producto principal (usando su ID)
-                },
+export async function POST(request) {
+  const { title, price, previewImg, condition, shipment, qualification, seller, categoryId, stock, description } = await request.json()//transformamos la peticion a json a js, y se almacenan en title y description
+  const newProduct = await prisma.product.create({//linea para crear datos en nuestra base de datos
+    data: {
+      title: title, //podria solo poner 1 vez title ya que asi llega, pero lo pongo 2 veces para que sea mas didactico
+      price: price,
+      // description: description,
+      previewImg: previewImg,
+      condition: condition,
+      shipment: shipment,
+      qualification: qualification,
+      seller: seller,
+      categoryId: categoryId,
 
-              },
-    })
-    return NextResponse.json(newProduct)
+      // categoryId: 1, // Asegúrate de proporcionar categoryId
+      ProductComplete: { // Si es una relación 1 a 1, puedes crear ProductComplete aquí
+        create: {
+          stock: 10,
+          description: description,
+        }
+      },
+
+    },
+    include: {
+      ProductComplete: true,
+    },
+  })
+  return NextResponse.json(newProduct)
 }

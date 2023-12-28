dfb8a2dc7ef16a915bcf7ab036d50407ca16239a
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-28 13:45:04 -0300

    add shippingSVG icon add some styles to ProductMain

--- src/containers/ProductMain.jsx
+++ src/containers/ProductMain.jsx
@@ -2,23 +2,36 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '@/components/Loading'
 import BadgetFreeShipping from '@/components/BadgetFreeShipping'
+import BadgetStars from '@/components/BadgetStars'
+import './ProductMain.css'
+import ShippingSVG from '@/SVG/ShippingSVG'
+
 
 const ProductMain = ({ product }) => {
-  // console.log(product)
+  console.log(product)
   return (
-    <div>
-      {!product && <Loading></Loading>}
+    <div className='product__container'>
+      {!product? <Loading></Loading>:<>
+      <header>
+      <p>{product?.condition}</p>
+      <BadgetStars qualification={product?.qualification}/>
+      </header>
 
-      <h1>{product?.title}</h1>
+      <p className='product__title'>{product?.title}</p>
+      <div className='product--img__contiainter'>
       <img className='product__img' src={product?.ProductComplete.images.image1} alt="" />
+      </div>
       <h4 className='prodcut__price'>${product?.price}</h4>
-      <p className='prodcut__shipment'>{product?.shipment == 0 ? <BadgetFreeShipping /> : '$' + product?.shipment}</p>
+      <ShippingSVG></ShippingSVG>
+      <p className='prodcut__shipment'>{product?.shipment == 0 ? <BadgetFreeShipping /> : +'$' + product?.shipment}</p>
       <div>{`Stock(${product?.ProductComplete.stock})`}</div>
-      <button>Add to cart</button>
-      <button>Buy</button>
+      <button className='btn btn__buy'>Buy now</button>
+      <button className='btn btn__cart'>Add to cart</button>
       <p>Description: {product?.ProductComplete.description}</p>
+      <p>Seller:{product?.seller}</p>
       <p>{product?.description}</p>
-
+      </>
+  }
 
     </div>
   )

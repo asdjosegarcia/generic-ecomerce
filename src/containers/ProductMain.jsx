80f65ba7cd80ff09c6b783e0261b555391e8b7fb
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-29 01:24:26 -0300

    add ProductMain styles

--- src/containers/ProductMain.jsx
+++ src/containers/ProductMain.jsx
@@ -11,27 +11,36 @@ const ProductMain = ({ product }) => {
   console.log(product)
   return (
     <div className='product__container'>
-      {!product? <Loading></Loading>:<>
-      <header>
-      <p>{product?.condition}</p>
-      <BadgetStars qualification={product?.qualification}/>
-      </header>
+      {!product ? <Loading></Loading> : <>
+        <header>
+          <p>{product?.condition}</p>
+          <div className='porduct--qualification__container'>
+            <p className='porduct--qualification__qualification-number'>{product?.qualification}</p>
+            <BadgetStars qualification={product?.qualification} />
+            <p>(560)</p>
+          </div>
+        </header>
 
-      <p className='product__title'>{product?.title}</p>
-      <div className='product--img__contiainter'>
-      <img className='product__img' src={product?.ProductComplete.images.image1} alt="" />
-      </div>
-      <h4 className='prodcut__price'>${product?.price}</h4>
-      <ShippingSVG></ShippingSVG>
-      <p className='prodcut__shipment'>{product?.shipment == 0 ? <BadgetFreeShipping /> : +'$' + product?.shipment}</p>
-      <div>{`Stock(${product?.ProductComplete.stock})`}</div>
-      <button className='btn btn__buy'>Buy now</button>
-      <button className='btn btn__cart'>Add to cart</button>
-      <p>Description: {product?.ProductComplete.description}</p>
-      <p>Seller:{product?.seller}</p>
-      <p>{product?.description}</p>
+        <p className='product__title'>{product?.title}</p>
+        <div className='product--img__contiainter'>
+          <img className='product__img' src={product?.ProductComplete.images.image1} alt="" />
+        </div>
+        <p className='prodcut__price'>${product?.price}</p>
+        <div className='product__shipment--container'>
+          <ShippingSVG width={'24px'} fill={"#696969"}></ShippingSVG>
+          <p className='prodcut__shipment'>{product?.shipment == 0 ? <BadgetFreeShipping /> : '‎ $' + product?.shipment}</p>
+        </div>
+        <p className='product__stock'>{`Stock(${product?.ProductComplete.stock})`}</p>
+        <button className='btn btn__buy'>Buy now</button>
+        <button className='btn btn__cart'>Add to cart</button>
+        <div>
+          <p className='product__description--title'>Description</p>
+          <p className='product__description--content'>{product?.ProductComplete.description}</p>
+        </div>
+        <p>Seller:{product?.seller}</p>
+        <p>{product?.description}</p>
       </>
-  }
+      }
 
     </div>
   )

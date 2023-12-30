1a86bdafd8589c329badc4934fbfed4a34dc5e9e
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-30 12:14:19 -0300

    add  LikeSVG DislikeSVG

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import LikeSVG from '@/SVG/LikeSVG'
+import DislikeSVG from '@/SVG/DislikeSVG'
 
 const QuestionCard = ({ product }) => {
-    console.log(product)
-  return (
-    <div className='question--card__container'>
-        {/* <img src="" alt="" />  para el dia que cree cuentas*/}
-        {/* <p className='question'>{product.question.question}</p> */}
-        <span>Like</span>
-        <span>Dislike</span>
+    console.log(product?.ProductComplete.question)
+    return (
+        <div className='question--card__container'>
+            {/* <img src="" alt="" />  para el dia que cree cuentas*/}
+            {/* <p className='question'>{product?.ProductComplete.question}</p> */}
+            <img src="" alt="" className='question--card__user-perfile-img' />
+            <LikeSVG height={'24'}  fill={'#3483fa'} /* filled={'#3483fa'} */></LikeSVG>
+            <DislikeSVG height={'24'}  fill={'#3483fa'} /* filled={'#3483fa'} */></DislikeSVG>
 
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default QuestionCard

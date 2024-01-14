fe7da44bf67485b0535b690cc450d8acc06c68d9
asdjosegarcia <asdjosegarcia@gmail.com>
2024-01-14 12:52:09 -0300

    add POST cart request to DB

--- src/app/api/cart/[id]/route.js
+++ src/app/api/cart/[id]/route.js
@@ -2,37 +2,35 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-const productId=1;
-// const user=2;
 export async function GET(request, { params }) {
     
-    const cart = await prisma.user.update({//
-        // where: { id: userId },
-        // include: { cart: true },
+    const cart = await prisma.cart.findUnique({//
+        where: { id:Number( params.id )},
         
-        where: { //were se usa para buscar lo que coincida
-            // id: Number(params.id),
-            data: {
-                products: {
-                    connect: [{ id: productId }],
-                },
-            }
-
-        },
-        // await db.cart.update({
-        //     where: { id: user.cart.id },
-        //     data: {
-        //       products: {
-        //         connect: [{ id: productId }],
-        //       },
-        //     },
-        // include: {
-        //     cart: true
-        // },
-
     })
     return NextResponse.json(cart)
 }
+
+// const user=2;
+// const productId=15;
+// export async function GET(request, { params }) {
+//     const updatedCart = await prisma.cart.update({
+        
+//         where: { id: Number(params.id) },//buscamos la id del carrito
+//         data: {//actualizamos el carrito
+//             products: {//seleccionamos la tabla product
+//                 connect: [{ id: productId }],//producto que conextamos, el producto debe existir, si el producto ya existe
+//             },
+//         },
+//     });
+//     return NextResponse.json(updatedCart)
+// }
+
+
+
+
+
+
 
 
 
--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -12,4 +12,21 @@ export async function GET(request, { params }){
     return NextResponse.json(carts)
 }
 
+export async function POST(request) {
+    const {userId,password,productId}=await request.json()
+    const user = await prisma.user.findUnique({//buscamos 1 unico item en la lista user
+        where: { id: userId },//cuando encuentra un user con la id userId..
+        include: { cart: true },//incluimos el carrito...
+      })
+
+    const updatedCart = await prisma.cart.update({
+        where: { id: user.cart.id },//buscamos la id del carrito, para asegurarnos de que el carrito corresponda a el usuario, extaemos la id del carrito del objeto usuario
+        data: {//actualizamos el carrito
+            products: {//seleccionamos la tabla product
+                connect: [{ id: productId }],//producto que conextamos, el producto debe existir, si el producto ya esta relacionado no se agrega de nuevo
+            },
+        },
+    });
+    return NextResponse.json(updatedCart)
+}
 

42c04796cb6f9672b8989e1a8e4bb07e66c3cc33
asdjosegarcia <asdjosegarcia@gmail.com>
2024-01-15 10:52:45 -0300

    change GET cart by id requests to cart requesto by userid

--- src/app/api/cart/[id]/route.js
+++ src/app/api/cart/[id]/route.js
@@ -4,27 +4,19 @@ const prisma = new PrismaClient()
 
 export async function GET(request, { params }) {
     
-    const cart = await prisma.cart.findUnique({//
+    const user = await prisma.user.findUnique({//
         where: { id:Number( params.id )},
-        
+        include: { cart: true },
     })
+    const cart = await prisma.cart.findUnique({//
+        where: { id:user.cart.id},
+        include: { products: true },
+    })
+
     return NextResponse.json(cart)
 }
 
-// const user=2;
-// const productId=15;
-// export async function GET(request, { params }) {
-//     const updatedCart = await prisma.cart.update({
-        
-//         where: { id: Number(params.id) },//buscamos la id del carrito
-//         data: {//actualizamos el carrito
-//             products: {//seleccionamos la tabla product
-//                 connect: [{ id: productId }],//producto que conextamos, el producto debe existir, si el producto ya existe
-//             },
-//         },
-//     });
-//     return NextResponse.json(updatedCart)
-// }
+
 
 
 

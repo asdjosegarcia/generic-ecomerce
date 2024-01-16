7f4421d363f90eae878f29760eab292d776303e4
asdjosegarcia <asdjosegarcia@gmail.com>
2024-01-16 12:01:29 -0300

    add  FavoriteSVG,DeleteSVG to CartCard

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import './CartCard.css'
 import CartInputNumber from './CartInputNumber'
+import HeartSVG from '@/SVG/HeartSVG'
+import HeartOutlineSVG from '@/SVG/HeartOutlineSVG'
+import DeleteSVG from '@/SVG/DeleteSVG'
 
 
 const CartCard = ({product}) => {
@@ -14,7 +17,10 @@ const CartCard = ({product}) => {
         <img className='cart-card__img' src={product.previewImg} alt=""/>
         <p className='cart-card__title'>{product.title}</p>
         <h4 className='cart-card__price'>${product.price}</h4>
-        {/* <span className='cart-card__favorite'>{(product.favorites)?<HeartSVG width={24} height={24} fill={'#3483fa'}/>:<HeartOutlineSVG width={24} height={24}fill={'#3483fa'}/>}</span> */}
+        <div className='cart-card__favorite-delete-container'>
+        <span className='cart-card__favorite'>{(product.favorites)?<HeartSVG width={24} height={24} fill={'#3483fa'}/>:<HeartOutlineSVG width={24} height={24}fill={'#3483fa'}/>}</span>
+        <span className='cart-card__delete'><DeleteSVG height={'24px'} fill={'#696969'}></DeleteSVG></span>
+        </div>
         {/* <span className='cart-card__qualification'><BadgetStars qualification={product.qualification}/></span> */}
         <p className='cart-card__shipment'>{product.shipment==0? <BadgetFreeShipping/>: '$'+ product.shipment }</p>
         {/* <p className='cart-card__condition' >{product.condition}</p> */}

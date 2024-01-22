15692c31e6c7e02cf9d9915e31ad592bce70ae67
asdjosegarcia <asdjosegarcia@gmail.com>
2024-01-22 03:59:17 -0300

    add notification to  QuestionCardMenu.jsx

--- src/components/organisms/QuestionCardMenu.jsx
+++ src/components/organisms/QuestionCardMenu.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import './QuestionCardMenu.css'
+import { variableContext } from "@/context/contexto";
+
+
 
 const QuestionCardMenu = () => {
+    const contexto = useContext(variableContext)
+
   return (
     <div className='question-card-menu__container'>
-        <span className='question-card-menu__report-comment'>Report Comment</span>
-        <span className='question-card-menu__report-comment'>Report Response</span>
+        <span onClick={()=>{contexto.setNotificationText('Reported')}} className='question-card-menu__report-comment'>Report Comment</span>
+        <span onClick={()=>{contexto.setNotificationText('Reported')}} className='question-card-menu__report-comment'>Report Response</span>
     </div>
 
   )

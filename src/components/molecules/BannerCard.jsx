11fdd6723b63253022b597f92acdc426caf59df5
asdjosegarcia <asdjosegarcia@gmail.com>
2024-02-01 13:42:08 -0300

    add partial styles to BannerCard

--- src/components/molecules/BannerCard.jsx
+++ src/components/molecules/BannerCard.jsx
@@ -4,7 +4,10 @@ import './BannerCard.css'
 const BannerCard = () => {
   return (
     <div className='banner-card__container'>
-        <img className='banner-card__img' src="" alt="" />
+      <div className='banner-card__cards-container'>
+        <img className='banner-card__img' src="/img/banners/summer-sale.jpg" alt="" />
+        <img className='banner-card__img' src="/img/banners/radeon-banner.webp" alt="" />
+      </div>
     </div>
   )
 }

35e78f53e350f330298d527518a9c357c23d7e10
asdjosegarcia <asdjosegarcia@gmail.com>
2024-02-02 00:20:22 -0300

    add  GET product by categoriId reiquest

---
+++ src/app/api/products/category/[categoryId]/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export async function GET(request, { params }) {
+    const productByCategory = await prisma.category.findMany({//encontrar todos los que conicidan
+        where: { //were se usa para buscar lo que coincida
+            id: Number(params.categoryId)//le decimos que busque lo que corresponda con lo que le mandamos en params.id
+        },include:{
+            products:true
+        }
+
+        // include: { ProductComplete: true },
+
+    })
+    console.log(productByCategory)
+    return NextResponse.json(productByCategory)//mostramos la tarea en pantalla
+}

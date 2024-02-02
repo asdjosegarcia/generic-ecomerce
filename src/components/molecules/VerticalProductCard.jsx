2a7010c3acea9d5163b8fe532a362c694cddebea
asdjosegarcia <asdjosegarcia@gmail.com>
2024-02-02 19:36:25 -0300

    add  discount calculator to VerticalProductCard

--- src/components/molecules/VerticalProductCard.jsx
+++ src/components/molecules/VerticalProductCard.jsx
@@ -3,16 +3,21 @@ import './VerticalProductCard.css'
 import BadgetFreeShipping from '../atoms/BadgetFreeShipping'
 
 const VerticalProductCard = (props) => {
-    // console.log('e')
+    let newPrice
+    if(props.product.discount>0){
+        const priceDiscount=props.product.price*props.product.discount/100
+        newPrice=(props.product.price-priceDiscount).toFixed(2)
+    }
+
     console.log(props.product)
     return (
         <div className='vertical-card__container'>
             <img className='vertical-card__img' src={props.product.previewImg}></img>
             <p>{props.product.title}</p>
-            <p className='vertical-card__old-price'>$300</p>
+            <p className='vertical-card__old-price'>${props.product.price}</p>
             <span className='vertical-card__price-container'>
-                <p className='vertical-card__price'>${props.product.price}&nbsp;</p>
-                <p className='vertical-card__price-discount'>23% OFF</p>
+                <p className='vertical-card__price'>${(newPrice)?newPrice:props.product.price}&nbsp;</p>
+                <p className='vertical-card__price-discount'>{(props.product.discount>0)?`${props.product.discount}% OFF`: ''}</p>
             </span>
             {(props.product.shipment==0)&&
             <BadgetFreeShipping></BadgetFreeShipping>

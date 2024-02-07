3a2b20c6c447b76bc42d36dfd99ee6f4a3f06224
asdjosegarcia <asdjosegarcia@gmail.com>
2024-02-07 15:33:08 -0300

    fix VerticalProductCard navigation

--- src/components/molecules/VerticalProductCard.jsx
+++ src/components/molecules/VerticalProductCard.jsx
@@ -2,32 +2,35 @@ import React from 'react'
 import './VerticalProductCard.css'
 import BadgetFreeShipping from '../atoms/BadgetFreeShipping'
 import BadgetPercentageDiscount from '../atoms/BadgetPercentageDiscount'
+import Link from 'next/link'
 
 const VerticalProductCard = (props) => {
     let newPrice
-    if(props.product.discount>0){
-        const priceDiscount=props.product.price*props.product.discount/100
-        newPrice=(props.product.price-priceDiscount).toFixed(2)
+    if (props.product.discount > 0) {
+        const priceDiscount = props.product.price * props.product.discount / 100
+        newPrice = (props.product.price - priceDiscount).toFixed(2)
     }
 
     // console.log(props.product)
     return (
-        <div className='vertical-card__container'>
-            <div className='vertical-card__img-container'>
-            <img className='vertical-card__img' src={props.product.previewImg}></img>
+        <Link href={`/product/${props.product.id}`}>
+            <div className='vertical-card__container'>
+                <div className='vertical-card__img-container'>
+                    <img className='vertical-card__img' src={props.product.previewImg}></img>
 
+                </div>
+                <p className='vertical-card__title'>{props.product.title}</p>
+                <p className='vertical-card__old-price'>${props.product.price}</p>
+                <span className='vertical-card__price-container'>
+                    <p className='vertical-card__price'>${(newPrice) ? newPrice : props.product.price}&nbsp;</p>
+                    <BadgetPercentageDiscount discount={props.product.discount}></BadgetPercentageDiscount>
+                </span>
+                {(props.product.shipment == 0) &&
+                    <BadgetFreeShipping></BadgetFreeShipping>
+                }
+                {/* <p>{props.product.shipment}$</p> */}
             </div>
-            <p className='vertical-card__title'>{props.product.title}</p>
-            <p className='vertical-card__old-price'>${props.product.price}</p>
-            <span className='vertical-card__price-container'>
-                <p className='vertical-card__price'>${(newPrice)?newPrice:props.product.price}&nbsp;</p>
-                <BadgetPercentageDiscount discount={props.product.discount}></BadgetPercentageDiscount>
-            </span>
-            {(props.product.shipment==0)&&
-            <BadgetFreeShipping></BadgetFreeShipping>
-            }
-            {/* <p>{props.product.shipment}$</p> */}
-        </div>
+        </Link>
     )
 }
 

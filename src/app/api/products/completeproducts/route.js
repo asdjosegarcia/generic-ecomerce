b31cbbfca5d3bcc652cce0ad05d14f0ab3b7bf99
asdjosegarcia <asdjosegarcia@gmail.com>
2024-02-28 18:25:41 -0300

    add PATCH request to UserPurchases

--- src/app/api/products/completeproducts/route.js
+++ src/app/api/products/completeproducts/route.js
@@ -164,7 +164,7 @@ export async function DELETE(request) {
     })
     response = "Product Deleted";
   } else {
-    response = "error"
+    response = "Prodcut not found"
   }
 
   return NextResponse.json(response);
--- src/app/api/user/purchases/route.js
+++ src/app/api/user/purchases/route.js
@@ -17,3 +17,42 @@ export async function POST(request) {
     console.log(user)
     return NextResponse.json(products)
 }
+
+export async function PATCH(request) {
+    const { userEmail,productId,units } = await request.json()
+    const user = await prisma.user.findUnique({
+        where: { email: userEmail },
+
+    })
+    const originalProduct=await prisma.product.findUnique({
+        where:{id:productId},
+        include:{
+            previewImgBase:true,
+        }
+
+    })
+    const userPurchases=await prisma.userPurchases.findUnique({
+        where:{userId:user.id},
+        include:{
+            purchasedProduct:true
+        }
+    })
+
+    const newPurchasedProduct=await prisma.purchasedProduct.create({
+        data: {
+            originalId:originalProduct.id,
+            title:originalProduct.title,
+            price:originalProduct.price,
+            condition:originalProduct.condition,
+            shipment:originalProduct.shipment,
+            previewImg:originalProduct.previewImg,
+            previewImgBase:originalProduct.previewImgBase.data.data,
+            previewImgMimetype:originalProduct.previewImgBase.mimetype,
+            units:units,
+            userPurchasesId:userPurchases.id
+            
+        }
+    })
+    // console.log(user)
+    return NextResponse.json(newPurchasedProduct)
+}

c47a6bb6a20e530df88cd9743daeec4074fd2b6c
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-05 15:50:45 -0300

    add paymentType parameter from userPurchases

--- src/components/organisms/CreditCardForm.jsx
+++ src/components/organisms/CreditCardForm.jsx
@@ -4,9 +4,34 @@ import { useSession } from "next-auth/react";
 import "./CreditCardForm.css";
 import PuchaseComplete from "@/components/organisms/PuchaseComplete";
 
-const CreditCardForm = () => {
+const CreditCardForm = (props) => {
   const { data: session } = useSession();
   const [getPuchaseComplete,setPuchaseComplete]=useState(null)//despues lo cambiamos por false
+ 
+  //add product to userPurchases
+  // console.log(props?.product?.id)
+  const request=async ()=>{
+    const res = await fetch(`/api/user/purchases`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userEmail:"user8@gmail.com",
+        productId:await props.product.id, // el await solo es para vitar cualquier bug 
+        paymentType:"credit card"
+      }),
+    });
+    const data = await res.json();
+    console.log(await data)
+    if(res.ok){
+      setPuchaseComplete(true)
+    }else{
+      alert('error')
+    }
+  }
+
+
 
   // console.log(session?.user?.name);
 
@@ -30,7 +55,7 @@ const CreditCardForm = () => {
       <img src="/img/mastercard-logo.png" alt="" />
       </div>
       {/* <input type="date" id="expiracion" name="expiracion" min="2022-01" max="2030-12"></input> */}
-      <button onClick={()=>setPuchaseComplete(true)} className="credit-card-form__confirm-button"  >Confirm </button>
+      <button onClick={()=>request()}  className="credit-card-form__confirm-button"  >Confirm </button>
     {getPuchaseComplete && (<PuchaseComplete username={session?.user?.name}></PuchaseComplete>)}
       
     </div>

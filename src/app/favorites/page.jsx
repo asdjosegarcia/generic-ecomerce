e5820cfd1fea164d048b040b45a6c130a30d43af
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-05 23:36:44 -0300

    add NothingHere to favorite Page

--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState, useContext } from 'react'
 import FavoritesList from '@/components/organisms/FavoritesList'
 import { useSession } from 'next-auth/react';
 import { variableContext } from "@/context/contexto";
+import NothingHere from '@/components/organisms/NothingHere';
+import HeartSVG from '@/SVG/HeartSVG';
+import PlusSVG from '@/SVG/PlusSVG';
 
 import Loading from '@/components/templates/Loading';
 
@@ -12,6 +15,7 @@ const page = () => {
 
     const { data: session } = useSession();//cargamos datos del usuario en session   
     const [getProducts, setProducts] = useState(null)//aqui cargaremos los datos del produto
+    const [getLoading,setLoading]=useState(true)
 
 
     useEffect(() => {
@@ -20,19 +24,27 @@ const page = () => {
                 .then(res => res.json())//tranformamos la respuesta a json y almacenamos en data
                 .then(data => {                    
                     setProducts(data.products)
+                    setLoading(false)
                     // console.log(data.products)
                 })
         } else {
 
         }
     }, [contexto.getNotificationText, contexto.getUserData])
+    console.log(getProducts)
     return (
         <>
-            {getProducts ?
+            {(getLoading)? 
+            <Loading/>
+            : 
+            <>
+            {(getProducts?.length>0)?
                 <FavoritesList products={getProducts}></FavoritesList>
                 :
-                <Loading/>
+                <NothingHere buttonText={"Add Favorites"} buttonIcon={<><PlusSVG/> &nbsp; <HeartSVG></HeartSVG></>  }/>
             }
+            </>
+        }
         </>
     )
 }
---
+++ src/components/organisms/NothingHere.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './NothingHere.css'
+import MainButton from '../atoms/MainButton'
+import Link from 'next/link'
+
+const NothingHere = (props) => {
+  return (
+    <div className='NothingHere'>
+      <div className='NothingHere__message-container'>
+        <p>There is nothing here.</p>
+        <p>Add something! </p>
+        <p>:)</p>
+        {/* <div className='NothingHere-message__buton-container'> */}
+        <Link href={"./"}>
+          <MainButton text={props.buttonText} icon={props.buttonIcon} />
+        </Link>
+        {/* </div> */}
+        {/* <button className='NothingHere-message__button'></button> */}
+        <></>
+      </div>
+    </div>
+  )
+}
+
+export default NothingHere

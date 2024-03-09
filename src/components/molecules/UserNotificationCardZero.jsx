e6180aaefba814f7966aef67e800c9cedcf51a00
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-09 17:14:00 -0300

    add createdAt to Notification Model

--- src/components/molecules/UserNotificationCardZero.jsx
+++ src/components/molecules/UserNotificationCardZero.jsx
@@ -4,13 +4,22 @@ import './UserNotificationCardZero.css'
 
 const UserNotificationCardZero = (props) => {
 
+  const date = new Date(props.notification.createdAt); //creamos un objeto con new date que tiene funciones
+  const options = {
+      weekday: "long", // 'short', 'long', 'narrow' como se va a mostrar el dia de la semana
+      year: "numeric", // '2-digit', 'numeric' como se va a mostrar el año
+      month: "numeric", // 'short', 'long', 'numeric', '2-digit' como se  va a mostrar el mes
+      day: "numeric", // '2-digit', 'numeric' como se va a mostrar el dia
+  };
+  const formatedDate = date.toLocaleDateString("en-US", options); //en-US formato de region, en este caso eeuu
+
     console.log(props.notification);
   return (
     <div className='UserNotificationCardZero'>
         <Link href={props.notification.link} className='UserNotificationCardZero__Link'>
         <h3 className='UserNotificationCardZero__title'>{props.notification.title}</h3>
         <p className='UserNotificationCardZero__description'>{props.notification.description}</p>
-        <span>Hoy</span>
+        <span className='UserNotificationCardZero__date'>{formatedDate}</span>
         </Link>
     </div>
   )

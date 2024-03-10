ebb4fc93df44c5d8b0d2a5716a91358d25efcbc0
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-10 02:37:10 -0300

    add viewNotification

--- src/app/api/user/notifications/route.js
+++ src/app/api/user/notifications/route.js
@@ -19,60 +19,75 @@ export async function POST(request) {
 }
 
 export async function PATCH(request) {
-    const { userEmail, type, title, description, icon, link } = await request.json()
+    //editamos una notificacion en particular o creamos una notificacion dentro de Notifications
+    const { userEmail, type, title, description, icon, link,id } = await request.json()
     const user = await prisma.user.findUnique({
         where: { email: userEmail },
         include: { notifications: true }
     })
+    if(!id){
+        let newNotification;
+        switch (true) {//al ser diferente notificaciones estas tendran tipos ej notificacion por compra sera tipo 20-29 y notificacion basica tipo 10-19, tipo texto 0-9
+            case (type == null):
+                break;
+            case (type < 10):
+                newNotification = await prisma.notification.create({
+                    data: {
+                        type: type,
+                        title: title,
+                        description: description,
+                        link: link,
+                        notificationsId: user.notifications.id
+                    }
+                })
+    
+                break;
+            case (type < 20):
+                newNotification = await prisma.notification.create({
+                    data: {
+                        type: type,
+                        title: title,
+                        description: description,
+                        icon: icon,//aun no se me ocurre la mejor forma de manejar iconos
+                        link: link,
+                        notificationsId: user.notifications.id
+                    }
+                })
+    
+                break;
+            case (type < 30):
+                newNotification = await prisma.notification.create({
+                    data: {
+                        type: type,
+                        title: title,
+                        description: description,
+                        icon: icon,//aun no se me ocurre la mejor forma de manejar iconos
+                        link: link,
+                        notificationsId: user.notifications.id
+                    }
+                })
+                break;
+            default:
+        }
+        return NextResponse.json(newNotification)
+    }else{
+        //buscamos la notificacion que corresponda segun el id y la marcamos con true
+        const viewNotificacion = await prisma.notification.update({ 
+            where: { id: id },
+            data:{
+                view:true
+            }
 
-    let newNotification;
-    switch (true) {//al ser diferente notificaciones estas tendran tipos ej notificacion por compra sera tipo 20-29 y notificacion basica tipo 10-19, tipo texto 0-9
-        case (type == null):
-            break;
-        case (type == 0):
-            newNotification = await prisma.notification.create({
-                data: {
-                    type: type,
-                    title: title,
-                    description: description,
-                    link: link,
-                    notificationsId: user.notifications.id
-                }
-            })
-
-            break;
-        case (type == 10):
-            newNotification = await prisma.notification.create({
-                data: {
-                    type: type,
-                    title: title,
-                    description: description,
-                    icon: icon,//aun no se me ocurre la mejor forma de manejar iconos
-                    link: link,
-                    notificationsId: user.notifications.id
-                }
-            })
-
-            break;
-        case (type == 20):
-            newNotification = await prisma.notification.create({
-                data: {
-                    type: type,
-                    title: title,
-                    description: description,
-                    icon: icon,//aun no se me ocurre la mejor forma de manejar iconos
-                    link: link,
-                    notificationsId: user.notifications.id
-                }
-            })
-            break;
-        default:
+        })
+        return NextResponse.json(viewNotificacion)
+        
     }
 
 
 
 
-    return NextResponse.json(newNotification)
+
+    
 }
 
 
--- src/components/molecules/UserNotificationCardZero.jsx
+++ src/components/molecules/UserNotificationCardZero.jsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
 import './UserNotificationCardZero.css'
+import { useRouter } from 'next/navigation';
 
+let cardStyle;
 const UserNotificationCardZero = (props) => {
-
+  const router = useRouter()
+  console.log(props.notification);
+  /////////////////////comprobacion sobre si el usuario vio la notificacion
+  if(props.notification.view){
+    cardStyle={"color":'gray'}
+  }
+  /////////////////////fecha
   const date = new Date(props.notification.createdAt); //creamos un objeto con new date que tiene funciones
   const options = {
       weekday: "long", // 'short', 'long', 'narrow' como se va a mostrar el dia de la semana
@@ -12,15 +20,28 @@ const UserNotificationCardZero = (props) => {
       day: "numeric", // '2-digit', 'numeric' como se va a mostrar el dia
   };
   const formatedDate = date.toLocaleDateString("en-US", options); //en-US formato de region, en este caso eeuu
+  /////////////////////redireccionamiento
+  const redirect=(e)=>{
+    e.preventDefault()
+      // fetch(`/api/favorites/${session.user.email}`)//realizamos una peticion get a parametro de la url.id
+      //     .then(res => res.json())//tranformamos la respuesta a json y almacenamos en data
+      //     .then(data => {
+      //         setProducts(data.products)
+      //         setLoading(false)
+      //         // console.log(data.products)
+      //     })
 
-    console.log(props.notification);
+    router.push(props.notification.link)
+
+  }
+
   return (
-    <div className='UserNotificationCardZero'>
-        <Link href={props.notification.link} className='UserNotificationCardZero__Link'>
+    <div className='UserNotificationCardZero' style={cardStyle}>
+        <a href={props.notification.link} onClick={redirect} className='UserNotificationCardZero__Link'>
         <h3 className='UserNotificationCardZero__title'>{props.notification.title}</h3>
         <p className='UserNotificationCardZero__description'>{props.notification.description}</p>
         <span className='UserNotificationCardZero__date'>{formatedDate}</span>
-        </Link>
+        </a>
     </div>
   )
 }

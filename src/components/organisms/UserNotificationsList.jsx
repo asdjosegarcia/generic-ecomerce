b24c5b84815f7bd79f6807acccef7a955d8d8f1a
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-10 12:24:21 -0300

    add order by time to UserNotificationList

--- src/components/organisms/UserNotificationsList.jsx
+++ src/components/organisms/UserNotificationsList.jsx
@@ -5,7 +5,10 @@ import UserNotificationCardZero from '../molecules/UserNotificationCardZero'
 
 const UserNotificationsList = (props) => {
   // console.log(props.notifications);
-  const notificationsToRender = props.notifications.map((notification, index) => {
+ const reversedNotifications= [...props.notifications].reverse()//invertimos la lista para tener las notificaciones nuevas primero
+
+
+  const notificationsToRender = reversedNotifications.map((notification, index) => {
     switch (true) {//al ser diferente notificaciones estas tendran tipos ej notificacion por compra sera tipo 20-29 y notificacion basica tipo 10-19, tipo texto 0-9
       case (notification.type == null):
         return <p key={index}>null</p>

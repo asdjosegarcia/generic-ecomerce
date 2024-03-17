92c67fff922ac133a29481dc7fe7bf0dd10c8eea
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-17 14:19:32 -0300

    fix CategoryVerticalCard imgSrc

--- src/components/molecules/CategoryVerticalCard.jsx
+++ src/components/molecules/CategoryVerticalCard.jsx
@@ -6,19 +6,21 @@ import './CategoryVerticalCard.css'
 let imgSrc;
 const CategoryVerticalCard = (props) => {
   const contexto = useContext(variableContext)
-  console.log("category",props.category);
-  if(props.category?.categoryImg?.imgUrl!== undefined){
+  // console.log("category",props.category);
+  if(props.category?.categoryImg?.imgUrl== null){
     imgSrc=contexto.bytesToBase(props.category?.categoryImg?.data.data,props.category?.categoryImg?.mimetype)
-    // console.log('data',props.category?.categoryImg);
+    console.log('data',props.category?.categoryImg);
   }else{
-    imgSrc='urlImg',props.category?.categoryImg?.imgUrl;
+    imgSrc=props.category?.categoryImg?.imgUrl;
+    console.log('url',props.category?.categoryImg);
+
   }
   return (
     <div className='CategoryVerticalCard CategoryVerticalCard__container '>
       <img className='CategoryVerticalCard__img' src={imgSrc}></img>
       <h1 className='CategoryVerticalCard__title'>{props.category.name}</h1>
     </div>
-  )
+  ) 
 }
 
 export default CategoryVerticalCard

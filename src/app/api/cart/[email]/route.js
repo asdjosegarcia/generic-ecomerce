ddec37dc0356532441b65f7979f454eb427acd59
asdjosegarcia <asdjosegarcia@gmail.com>
2024-03-27 07:09:35 -0300

    add  preview-img-base64 to GET-Favorites-products-by-email enpoint

--- src/app/api/cart/[email]/route.js
+++ src/app/api/cart/[email]/route.js
@@ -1,20 +1,21 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+//deshabilitado por falta de seguridad
+// import { NextResponse } from "next/server";
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
 
-export async function GET(request, { params }) {
+// export async function GET(request, { params }) {
     
-    const user = await prisma.user.findUnique({//
-        where: { email: params.email },
-        include: { cart: true },
-    })
-    const cart = await prisma.cart.findUnique({//
-        where: { id:user.cart.id},
-        include: { products: true },
-    })
+//     const user = await prisma.user.findUnique({//
+//         where: { email: params.email },
+//         include: { cart: true },
+//     })
+//     const cart = await prisma.cart.findUnique({//
+//         where: { id:user.cart.id},
+//         include: { products: true },
+//     })
 
-    return NextResponse.json(cart)
-}
+//     return NextResponse.json(cart)
+// }
 
 
 
--- src/app/api/favorites/route.js
+++ src/app/api/favorites/route.js
@@ -3,29 +3,53 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function POST(request) {
-    const {email,password,productId}=await request.json()
-    const user = await prisma.user.findUnique({//buscamos 1 unico item en la lista user
-        where: { email: email },//cuando encuentra un user con la id userId..
-        include: { favorites: true },//incluimos lista favoritos
-      })
+    const { email, password, productId } = await request.json()
+    if (productId !== undefined) {
+        const user = await prisma.user.findUnique({//buscamos 1 unico item en la lista user
+            where: { email: email },//cuando encuentra un user con la id userId..
+            include: { favorites: true },//incluimos lista favoritos
+        })
 
-    const updatedFovorites = await prisma.favorites.update({
-        where: { id: user.favorites.id },//buscamos la id de favoritos, para asegurarnos de que el carrito corresponda a el usuario, extaemos la id del carrito del objeto usuario
-        data: {//actualizamos el favoritos
-            products: {//seleccionamos la tabla product
-                connect: [{ id: productId }],//producto que conectamos, el producto debe existir, si el producto ya esta relacionado no se agrega de nuevo
+        const updatedFovorites = await prisma.favorites.update({
+            where: { id: user.favorites.id },//buscamos la id de favoritos, para asegurarnos de que el carrito corresponda a el usuario, extaemos la id del carrito del objeto usuario
+            data: {//actualizamos el favoritos
+                products: {//seleccionamos la tabla product
+                    connect: [{ id: productId }],//producto que conectamos, el producto debe existir, si el producto ya esta relacionado no se agrega de nuevo
+                },
             },
-        },
-    });
-    return NextResponse.json(updatedFovorites)
+        });
+        return NextResponse.json(updatedFovorites)
+    } else {
+        const user = await prisma.user.findUnique({//
+            where: { email: email },
+            include: {
+                favorites: {
+                    include: {
+                        products: {
+                            include: {
+                                previewImgBase: true
+                            }
+                        }
+                    }
+                }
+            },
+        })
+        // const favorites = await prisma.favorites.findUnique({//
+        //     where: { id:user.favorites.id},
+        //     include: { products: true },
+        // })
+
+        return NextResponse.json(user.favorites)
+    }
 }
 
+
 export async function DELETE(request) {
-    const {email,password,productId}=await request.json()
+    const { email, password, productId } = await request.json()
     const user = await prisma.user.findUnique({//buscamos 1 unico item en la lista user
         where: { email: email },//cuando encuentra un user con el email..
         include: { favorites: true },//incluimos el carrito...
-      })
+    })
 
     const updatedFavorites = await prisma.favorites.update({
         where: { id: user.favorites.id },

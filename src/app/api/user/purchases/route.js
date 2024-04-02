df9392fee51aea5dd7a8cc72a5805e6dfe0a7f70
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-02 01:17:41 -0300

    add ADD-multiple-products endpoint request

--- src/app/api/user/purchases/route.js
+++ src/app/api/user/purchases/route.js
@@ -19,57 +19,121 @@ export async function POST(request) {
 }
 
 export async function PATCH(request) {
-    const { userEmail,productId,units,paymentType } = await request.json()
-    const user = await prisma.user.findUnique({
-        where: { email: userEmail },
-        include: { notifications: true }//es necesario para crear la notificacion del producto
+    const { userEmail,productId,units,paymentType,products } = await request.json()
+    if(products){
+        console.log();
 
-    })
-    const originalProduct=await prisma.product.findUnique({
-        where:{id:productId},
-        include:{
-            previewImgBase:true,
-        }
+        const user = await prisma.user.findUnique({
+            where: { email: userEmail },
+            include: { notifications: true }//es necesario para crear la notificacion del producto
+        })
+        
+        products.map( async(product)=>{
+            const originalProduct=await prisma.product.findUnique({
+                where:{id:product.productId},
+                include:{
+                    previewImgBase:true,
+                }
+            })
 
-    })
-    const userPurchases=await prisma.userPurchases.findUnique({
-        where:{userId:user.id},
-        include:{
-            purchasedProduct:true
-        }
-    })
+            const userPurchases=await prisma.userPurchases.findUnique({
+                where:{userId:user.id},
+                include:{
+                    purchasedProduct:true
+                }
+            })
 
-    const newPurchasedProduct=await prisma.purchasedProduct.create({
-        data: {
-            originalId:originalProduct.id,
-            title:originalProduct.title,
-            price:originalProduct.price,
-            condition:originalProduct.condition,
-            shipment:originalProduct.shipment,
-            seller:originalProduct.seller,
-            paymentType:paymentType,
-            previewImg:originalProduct.previewImg,
-            previewImgBase:originalProduct.previewImgBase.data,
-            previewImgMimetype:originalProduct.previewImgBase.mimetype,
-            units:units,
-            userPurchasesId:userPurchases.id
-        }
-    })
+            const newPurchasedProduct=await prisma.purchasedProduct.create({
+                data: {
+                    originalId:originalProduct.id,
+                    title:originalProduct.title,
+                    price:originalProduct.price,
+                    condition:originalProduct.condition,
+                    shipment:originalProduct.shipment,
+                    seller:originalProduct.seller,
+                    paymentType:paymentType,
+                    previewImg:originalProduct.previewImg,
+                    previewImgBase:originalProduct.previewImgBase.data,
+                    previewImgMimetype:originalProduct.previewImgBase.mimetype,
+                    units:product.units,
+                    userPurchasesId:userPurchases.id
+                }
+            })
 
-    const newProductNotification = await prisma.notification.create({//creamos una notificacion en las notificaciones del usuario, indicando que el producto se compro
-        data: {
-            type: 20,
-            title:"Purchase completed!",
-            description: `Did you buy ${newPurchasedProduct.title} x${newPurchasedProduct.units} `,
-            link: '/user/purchases',
-            notificationsId: user.notifications.id,
-            product:{
-                connect:[{id:productId}]
+            const newProductNotification = await prisma.notification.create({//creamos una notificacion en las notificaciones del usuario, indicando que el producto se compro
+                data: {
+                    type: 20,
+                    title:"Purchase completed!",
+                    description: `Did you buy ${newPurchasedProduct.title} x${newPurchasedProduct.units} `,
+                    link: '/user/purchases',
+                    notificationsId: user.notifications.id,
+                    product:{
+                        connect:[{id:productId}]
+                    }
+                }
+            })
+
+
+
+        })
+
+
+        return NextResponse.json('ok')
+        
+
+
+
+    }else{ //esto tendria que retirarse, pero no me dan los tiempos para modificar el codigo del frontend
+        const user = await prisma.user.findUnique({
+            where: { email: userEmail },
+            include: { notifications: true }//es necesario para crear la notificacion del producto
+        })
+        const originalProduct=await prisma.product.findUnique({
+            where:{id:productId},
+            include:{
+                previewImgBase:true,
             }
-        }
-    })
-    // console.log(user)
-    return NextResponse.json(newPurchasedProduct)
+    
+        })
+        const userPurchases=await prisma.userPurchases.findUnique({
+            where:{userId:user.id},
+            include:{
+                purchasedProduct:true
+            }
+        })
+    
+        const newPurchasedProduct=await prisma.purchasedProduct.create({
+            data: {
+                originalId:originalProduct.id,
+                title:originalProduct.title,
+                price:originalProduct.price,
+                condition:originalProduct.condition,
+                shipment:originalProduct.shipment,
+                seller:originalProduct.seller,
+                paymentType:paymentType,
+                previewImg:originalProduct.previewImg,
+                previewImgBase:originalProduct.previewImgBase.data,
+                previewImgMimetype:originalProduct.previewImgBase.mimetype,
+                units:units,
+                userPurchasesId:userPurchases.id
+            }
+        })
+    
+        const newProductNotification = await prisma.notification.create({//creamos una notificacion en las notificaciones del usuario, indicando que el producto se compro
+            data: {
+                type: 20,
+                title:"Purchase completed!",
+                description: `Did you buy ${newPurchasedProduct.title} x${newPurchasedProduct.units} `,
+                link: '/user/purchases',
+                notificationsId: user.notifications.id,
+                product:{
+                    connect:[{id:productId}]
+                }
+            }
+        })
+        // console.log(user)
+        return NextResponse.json(newPurchasedProduct)
+    }
 }
 
 export async function DELETE(request) {

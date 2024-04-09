7bbf3c2c39bd52e23d04f2c7a78cabdaf0ac6936
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-09 17:02:18 -0300

    add loading to login,register pages

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,15 +1,18 @@
 "use client"
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form";//para el manejo del formulario
 import { signIn } from 'next-auth/react'; //para el envio del formulario a la DB
 import { useRouter } from 'next/navigation'; //para la redirecciones
+import Loading from '@/components/templates/Loading'
 import "./LoginPage.css"
 
 
 const LoginPage = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()//register para el manejo de los inputs, errors para el manejo de los errores, handleSubmit
   const router = useRouter()//para las redirecciones
-  const [error,setError]=useState(null)//para almacenar el errror
+  const [error, setError] = useState(null)//para almacenar el errror
+  const [getLoading, setLoading] = useState(false)
+
 
   const onSubmit = handleSubmit(async data => { //data es la informacion que tomamos del usuario
     const res = await signIn('credentials', {//signIn es una funcion de next-auth para el logueo, ya sea con google,facebook etc, por defecto nos redirecciona
@@ -21,6 +24,7 @@ const LoginPage = () => {
     if (res.error) {
       // alert(res.error);
       setError(res.error)
+      setLoading(false)
     } else {
       // console.log('enviando a /dashboard')
       router.push('/') //si la autenticacion sale bien redireccionamos a el usuario a dashboard
@@ -31,33 +35,37 @@ const LoginPage = () => {
 
 
   return (
-    <div className='login--form__container'>
-      <form onSubmit={onSubmit} className=''>{/* una vez completado el formulario ejecuta la funcion onSubmit, que ejecuta handle submit */}
-        <h1 className="">Login</h1>
-        <label htmlFor="email" className="">Email</label>{/* texto superior */}
-        <input type="text" {...(register("email", { required: { value: true, message: 'Email is required' } }))} placeholder="yourUser123@email.com" className="" />
-        {
-          errors.email && (//si error.email existe
-          <span className="">{errors.email.message}</span>//se crea este span
-          )
-        }
-        <br></br>
-        <label htmlFor="password" className="">Password</label>{/* texto superior */}
-        <input type="password" {...(register("password", { required: { value: true, message: 'Password is required' } }))} placeholder="******" className="" />
-        {
-          errors.password && (//si error.password existe
-          <span className="">{errors.password.message}</span>//se crea este span
-          )
-        }
-        {error && (
-          <p className=''>{error}</p>
-        )}
-        <button className="">
-          Login
-        </button>
-        <button className='login--form__register' onClick={()=>{router.push('/auth/register')}}>Register</button>
-      </form>
-    </div>
+    <>
+      {getLoading && <Loading backgroundColor={"rgba(160, 160, 160, 0.164)"} />}
+
+      <div className='login--form__container'>
+        <form onSubmit={onSubmit} className=''>{/* una vez completado el formulario ejecuta la funcion onSubmit, que ejecuta handle submit */}
+          <h1 className="">Login</h1>
+          <label htmlFor="email" className="">Email</label>{/* texto superior */}
+          <input type="text" {...(register("email", { required: { value: true, message: 'Email is required' } }))} placeholder="yourUser123@email.com" className="" />
+          {
+            errors.email && (//si error.email existe
+              <span className="">{errors.email.message}</span>//se crea este span
+            )
+          }
+          <br></br>
+          <label htmlFor="password" className="">Password</label>{/* texto superior */}
+          <input type="password" {...(register("password", { required: { value: true, message: 'Password is required' } }))} placeholder="******" className="" />
+          {
+            errors.password && (//si error.password existe
+              <span className="">{errors.password.message}</span>//se crea este span
+            )
+          }
+          {error && (
+            <p className=''>{error}</p>
+          )}
+          <button className="" onClick={() => { setLoading(true) }}>
+            Login
+          </button>
+          <button className='login--form__register' onClick={() => { router.push('/auth/register') }}>Register</button>
+        </form>
+      </div>
+    </>
   )
 }
 
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,98 +1,107 @@
 "use client"
-import react,{useState} from "react";
+import react, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import "./RegisterPage.css"
+import Loading from '@/components/templates/Loading'
 
 
 
 
-const Registerpage=()=>{
-    const {register,handleSubmit, formState:{errors}}=useForm()//descomponemos el objeto devuelto por la funcion useForm()
-        //register para poder acceder a las funcionalidades de useForm
-        //handlesubmit que cancela el comportamiento por defecto
-        //formState objeto que tiene informacion del formulario, si se envio, si tiene algun error,
-        //objeto que alamcena los datos que extrae formState
-    const router=useRouter()//para redirigir a el usuario
-  const [error,setError]=useState(null)//para almacenar el errror
-    const [getPasswordNoMatch,setPasswordNoMatch]=useState(null)
+
+const Registerpage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()//descomponemos el objeto devuelto por la funcion useForm()
+    //register para poder acceder a las funcionalidades de useForm
+    //handlesubmit que cancela el comportamiento por defecto
+    //formState objeto que tiene informacion del formulario, si se envio, si tiene algun error,
+    //objeto que alamcena los datos que extrae formState
+    const router = useRouter()//para redirigir a el usuario
+    const [error, setError] = useState(null)//para almacenar el errror
+    const [getLoading, setLoading] = useState(false)
+    const [getPasswordNoMatch, setPasswordNoMatch] = useState(null)
 
 
-    const onSubmit=handleSubmit(async(data)=>{//
+    const onSubmit = handleSubmit(async (data) => {//
         // console.log(data) datos que enviamos
-        
-        if(data.password !== data.confirmPassword){//comprobamos si contraseña y confirmarcontraseña no coinciden
+
+        if (data.password !== data.confirmPassword) {//comprobamos si contraseña y confirmarcontraseña no coinciden
             // alert("password do not match")
             setPasswordNoMatch("Password does not match")
-        }else{
-            const res=await fetch('/api/auth/register',{//enviamos los datos a register
-                method:'POST',
-                body:JSON.stringify({//transformamos el objeto a JSON
-                    username:data.username,//enviamos los datos de data por separado
-                    email:data.email,
-                    password:data.password
+        } else {
+            const res = await fetch('/api/auth/register', {//enviamos los datos a register
+                method: 'POST',
+                body: JSON.stringify({//transformamos el objeto a JSON
+                    username: data.username,//enviamos los datos de data por separado
+                    email: data.email,
+                    password: data.password
                 }),
-                headers:{
-                    'Content-type':'application/json'
+                headers: {
+                    'Content-type': 'application/json'
                 }
             })
-                if(res.ok ){//comprobamos respuesta
-                        router.push('/auth/login')//si todo sale bien al ejecutar la funcion redirifimos a el usuario a el Login
-                }else{
-                     const errorResponse= await res.json()
-                     setError(errorResponse.message)
-                     console.log(errorResponse.message)
-                }
+            if (res.ok) {//comprobamos respuesta
+                router.push('/auth/login')//si todo sale bien al ejecutar la funcion redirifimos a el usuario a el Login
+            } else {
+                const errorResponse = await res.json()
+                setError(errorResponse.message)
+                console.log(errorResponse.message)
+                setLoading(false)
+
+            }
         }
     })
-    
-    return(
-       /*  <secition className='register--form__section'> */
-        <div className="register--form__container" >
-            <form onSubmit={onSubmit}>
-                <h2>Sing up</h2>
-                <label>Username</label>
-                <input type="text" {...(register("username", { required: { value: true, message: 'Username is required' } }))} placeholder="user123"></input>
-                {/*  
+
+
+
+    return (
+        /*  <secition className='register--form__section'> */
+        <>
+            {getLoading && <Loading backgroundColor={"rgba(160, 160, 160, 0.164)"} />}
+            <div className="register--form__container" >
+                <form onSubmit={onSubmit}>
+                    <h2>Sing up</h2>
+                    <label>Username</label>
+                    <input type="text" {...(register("username", { required: { value: true, message: 'Username is required' } }))} placeholder="user123"></input>
+                    {/*  
                 required:  //requerimientos del input
                 value:true //requerimos que el parametro sea obligatorio
                 message:'' //en caso de no cumplirse el requerimiento carga message, el error se carga en errors.message
                 ...        //desgloza las propiedades devueltas por el objeto register
                 "username" //nombre del campo
                 */}
-                {
-                    errors.username && (//error que se genera en username
-                        <span>{errors.username.message}</span>//mostramos el error que cargamos desde el span
-                    )
-                }
-                <br></br>
+                    {
+                        errors.username && (//error que se genera en username
+                            <span>{errors.username.message}</span>//mostramos el error que cargamos desde el span
+                        )
+                    }
+                    <br></br>
 
-                <label>Email</label>
-                <input type="email" {...(register("email", {required: { value: true, message: 'Email is required' }  }))} placeholder="user@mail.com"></input>
-                {
-                    errors.email && (
-                        <span>{errors.email.message}</span>
-                    )
-                }
-                <br></br>
-                <label>Password</label>
-                <input className="register--form__password-input" type="password" {...(register("password", {required: { value: true, message: 'Password is required' }  }))} placeholder="*******"></input>
-                {!errors.confirmPassword && getPasswordNoMatch?(<span>{getPasswordNoMatch}</span>):(<span>{errors.password?.message}</span>)}
-                <br></br>
-                <label>Confirm Password</label>
-                <input className="register--form__password-input" type="password" {...(register("confirmPassword", {required: { value: true, message: 'Confirm Password is required' }}))} placeholder="*******"></input>
-                {!errors.confirmPassword && getPasswordNoMatch?(<span>{getPasswordNoMatch}</span>):(<span>{errors.confirmPassword?.message}</span>)}
-                {/* operador ternario para mostrar uno u otro mensaje */}
+                    <label>Email</label>
+                    <input type="email" {...(register("email", { required: { value: true, message: 'Email is required' } }))} placeholder="user@mail.com"></input>
+                    {
+                        errors.email && (
+                            <span>{errors.email.message}</span>
+                        )
+                    }
+                    <br></br>
+                    <label>Password</label>
+                    <input className="register--form__password-input" type="password" {...(register("password", { required: { value: true, message: 'Password is required' } }))} placeholder="*******"></input>
+                    {!errors.confirmPassword && getPasswordNoMatch ? (<span>{getPasswordNoMatch}</span>) : (<span>{errors.password?.message}</span>)}
+                    <br></br>
+                    <label>Confirm Password</label>
+                    <input className="register--form__password-input" type="password" {...(register("confirmPassword", { required: { value: true, message: 'Confirm Password is required' } }))} placeholder="*******"></input>
+                    {!errors.confirmPassword && getPasswordNoMatch ? (<span>{getPasswordNoMatch}</span>) : (<span>{errors.confirmPassword?.message}</span>)}
+                    {/* operador ternario para mostrar uno u otro mensaje */}
+                    {
+                        error && (<p>{error}</p>)
+                    }
+                    <button onClick={() => { setLoading(true) }}>Register</button>
+                    <button className="register--form__login" onClick={() => router.push('/auth/login')}>Login</button>
 
-                {
-                    error && (<p>{error}</p>)
-                }
-                <button>Register</button>
-                <button className="register--form__login" onClick={()=>{router.push('/auth/login')}}>Login</button>
-                
 
-            </form>
-        </div>
+                </form>
+            </div>
+        </>
     )
 }
 export default Registerpage

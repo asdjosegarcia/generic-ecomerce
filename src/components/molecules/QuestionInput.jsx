6c164fd99eb27c44cc707c858054a1f73493ed67
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-10 15:05:38 -0300

    add questions redirection to no logged users

--- src/components/molecules/QuestionInput.jsx
+++ src/components/molecules/QuestionInput.jsx
@@ -2,14 +2,17 @@ import React, { useState, useContext } from 'react'
 import './QuestionInput.css'
 import { useSession } from 'next-auth/react';
 import { variableContext } from "@/context/contexto";
+import { useRouter } from 'next/navigation'
 
 
-// let questionValue = ''//variable para almacenar el valor del textarea
+
 const QuestionInput = (props) => {
     const contexto = useContext(variableContext)
     const { data: session } = useSession();//cargamos datos del usuario en session
     const [getMessage, setMessage] = useState('');
-    const [getTextareaValue,setTextareaValue]=useState('')
+    const [getTextareaValue, setTextareaValue] = useState('')
+    const router = useRouter();
+
 
 
 
@@ -32,26 +35,24 @@ const QuestionInput = (props) => {
             });
             if (res.ok) {
                 contexto.setNotificationText('Question added')
-                setTextareaValue('') 
+                setTextareaValue('')
                 contexto.setNewQuestion(
                     {
-                        username:name,
+                        username: name,
                         question: getTextareaValue,
                         like: 0,
-                        dislike:0,
+                        dislike: 0,
                     }
                 )
                 // contexto.addNew
             } else {
                 contexto.setNotificationText('Error')
             }
-            // console.log(name,questionValue,props.porduct.id)
-            // console.log(props)
 
         } else {
+            router.push('/auth/login')
             setMessage('The question cannot be empty')
         }
-        // console.log(questionValue)
     }
 
 
@@ -59,12 +60,12 @@ const QuestionInput = (props) => {
         <>
             <form className="question-input__container" onSubmit={sendQuestion}>
                 <div className='question-input__text-area-container'>
-                 <textarea type="text" placeholder="Ask a question" className="question-input__input" value={getTextareaValue} maxLength={90} rows="2"  onChange={(e) => { setTextareaValue(e.target.value)   }} />
-                <p className='question-input__caracters-number'>{getTextareaValue.length}/90</p>
+                    <textarea type="text" placeholder="Ask a question" className="question-input__input" value={getTextareaValue} maxLength={90} rows="2" onChange={(e) => { setTextareaValue(e.target.value) }} />
+                    <p className='question-input__caracters-number'>{getTextareaValue.length}/90</p>
                 </div>
-                <button className="question-input__button">Send</button>
+                <button className="question-input__button" >Send</button>
             </form>
-            <span style={{ color: 'red',textAlign:'center' }}>{getMessage}</span>
+            <span style={{ color: 'red', textAlign: 'center' }}>{getMessage}</span>
         </>
     )
 }

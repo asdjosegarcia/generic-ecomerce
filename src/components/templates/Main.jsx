86addc0bc3de5a9ff2c672a0d73194089bae6a48
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-12 01:01:01 -0300

    add desktop banner style

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -18,12 +18,14 @@ const Main = () => {
   return (
     <div  className='main__container'>
       <MainBanner></MainBanner>
+      <div className='main_products'>
       <HorizontalProductList  title={'Stuff that might interest you!'} link={'/api/products/category/1'}></HorizontalProductList>
       <HorizontalProductList  title={'Smartphones'} link={'/api/products/category/2'}></HorizontalProductList>
       <VerticalProductList title={'Offers'} link={'/api/offers'}></VerticalProductList>  
       <ProductList title={'Waiting for you'} link={'/api/products/category/3'}/>      
       {/* <VerticalProductList title={'Categories'} link={'/api/products/category/5'}></VerticalProductList>   */}
       <HorizontalProductList  title={'Your history'} link={'/api/products/category/4'}></HorizontalProductList>
+      </div>
 
     </div>
   )

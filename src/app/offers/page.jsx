5ef5cc94ae4b36e57ea4d4f96042160e5f0c2fb7
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-19 19:00:37 -0300

    fix desktop styles to offers-page

--- src/app/offers/page.jsx
+++ src/app/offers/page.jsx
@@ -7,28 +7,28 @@ import Loading from '@/components/templates/Loading'
 
 const page = () => {
   const [getLoading,setLoading]=useState(true)
-  const [getDesktop,setDesktop]=useState(false)
-    //////////////////////detectamos tamaño de apntalla, por lo general para saber si estamos en desktop o mobile
-    const windowSize = () => {
-      if (window.innerWidth > 800) {//si es mayor a 800 estamos en desktop
-        // desktop = true
-        setDesktop(true)
-      } else {
-        setDesktop(false)
-      }
-    }
-    if (typeof window !== 'undefined') {// si windows no es un defined, esto para evitar errores al generar el build, ya que da problemas por no estar en un navegaor sino en el servidor
-      window.addEventListener('resize', function (event) { //detecta cuando el tamaño de la pantalla cambia y ejecuta
-        windowSize()
-      });
-    }
+  // const [getDesktop,setDesktop]=useState(false)
+    // //////////////////////detectamos tamaño de apntalla, por lo general para saber si estamos en desktop o mobile
+    // const windowSize = () => {
+    //   if (window.innerWidth > 800) {//si es mayor a 800 estamos en desktop
+    //     // desktop = true
+    //     setDesktop(true)
+    //   } else {
+    //     setDesktop(false)
+    //   }
+    // }
+    // if (typeof window !== 'undefined') {// si windows no es un defined, esto para evitar errores al generar el build, ya que da problemas por no estar en un navegaor sino en el servidor
+    //   window.addEventListener('resize', function (event) { //detecta cuando el tamaño de la pantalla cambia y ejecuta
+    //     windowSize()
+    //   });
+    // }
 
   return (
 
         <div className='OffersPage OffersPage__container'>
           {getLoading && <Loading backgroundColor={"rgba(160, 160, 160, 0.164)"}/>}
           <img className='OffersPage__banner-img' src='/img/banners/black-friday-1.jpg'></img>
-          <CategoriesCardList getDesktop={getDesktop} link={'/api/categories/'} setLoading={setLoading}></CategoriesCardList>
+          <CategoriesCardList /* getDesktop={getDesktop} */ link={'/api/categories/'} setLoading={setLoading}></CategoriesCardList>
         </div>
       
   )
